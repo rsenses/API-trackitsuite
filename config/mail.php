131467f924e08cead637c0226842305faab4162f
Rubén Silva <ruben.silva@rsenses.com>
2018-11-28 11:31:42 +0100

    nuevo from name genérico ONCE

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'noreply@trackitsuite.com'),
-        'name' => env('MAIL_FROM_NAME', 'Trackit'),
+        'name' => env('MAIL_FROM_NAME', 'Ciclo Foro Inserta'),
     ],
 
     /*
